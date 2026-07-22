ba12767e5a15fc909c71fa8052f6106b3171dd35
eda <eda@localhost.localdomain>
2026-07-22 09:09:22 -0400

    signoff(fm): Directory assembly契约(option-2, 8黑盒=L2Directory绿target depends_on+7 array_ext vendor宏, vmucp验16对称死位)

--- scripts/sidecar/fm_sidecar_verdict.py
+++ scripts/sidecar/fm_sidecar_verdict.py
@@ -146,7 +146,7 @@ def appvar_spec_err(av, mode, relaxed_declared, target):
     _MU_STRENGTHEN = {"LoadQueueUncache",
                       "FastArbiter_46", "FastArbiter_47", "FastArbiter_27", "FastArbiter_44",
                       "ICacheCtrlUnit", "ICacheDataArray", "IPrefetchPipe",
-                      "DivUnit", "FDivSqrt", "InstrMMIOEntry", "InstrUncache", "TXDAT_4", "FAlu", "FCVT", "IssueQueueStdMoud", "MulUnit", "TXREQ", "TlbStorageWrapper", "TlbStorageWrapper_1", "IssueQueueStaMou", "IssueQueueLdu", "TXDAT", "Scheduler_1", "Scheduler", "Scheduler_3", "MSHR", "TageBTable"}
+                      "DivUnit", "FDivSqrt", "InstrMMIOEntry", "InstrUncache", "TXDAT_4", "FAlu", "FCVT", "IssueQueueStdMoud", "MulUnit", "TXREQ", "TlbStorageWrapper", "TlbStorageWrapper_1", "IssueQueueStaMou", "IssueQueueLdu", "TXDAT", "Scheduler_1", "Scheduler", "Scheduler_3", "MSHR", "TageBTable", "Directory"}
     expected_mu = "true" if target in _MU_STRENGTHEN else "false"
     if av[muk] != expected_mu:
         return f"target_strengthening:{muk}:{target}={av[muk]}_expected_{expected_mu}"
